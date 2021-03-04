c3d0722deb785775cac0574e4cc0309b52ad7894
Michel Pereira <michel.augusto@pg.inatel.br>
2021-03-04 08:00:57 -0300

    feat: add assertion-library

---
+++ assertion-library.js
@@ -0,0 +1,35 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+test("sum adds numbers", () => {
+  const result = sum(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
+
+test("subtract subtracts numbers", () => {
+  const result = subtract(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`✔ ${title}`);
+  } catch (error) {
+    console.error(`𐄂 ${title}`);
+    console.error(error);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+  };
+}
